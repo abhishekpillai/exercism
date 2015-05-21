92be7894c994edb2096e85e4486c4cec4301eeaf
Abhi Pillai <abhi@groupon.com>
2015-05-21 09:16:12 -0500

    WIP: anagram

---
+++ javascript/anagram/anagram.js
@@ -0,0 +1,19 @@
+/*
+* Write a program that, given a word and a list of possible anagrams, selects
+* the correct sublist.
+*
+* Given `"listen"` and a list of candidates like `"enlists" "google" "inlets"
+* "banana"` the program should return a list containing `"inlets"`.
+*/
+
+var Anagram = function(word) {
+  this.word = word;
+};
+
+function Anangram(word) {
+  this.word = word;
+};
+
+
+module.exports = Anagram;
+
