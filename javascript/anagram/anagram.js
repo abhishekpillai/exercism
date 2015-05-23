7f05930b6f0655462896a441ca3f17455cf76225
Abhi Pillai <abhi@groupon.com>
2015-05-23 17:40:46 -0500

    Anagram JS: one more test to pass

--- javascript/anagram/anagram.js
+++ javascript/anagram/anagram.js
@@ -6,14 +6,41 @@
 * "banana"` the program should return a list containing `"inlets"`.
 */
 
-var Anagram = function(word) {
-  this.word = word;
+var matcher = {
+  matches: function(candidates) {
+    if(typeof(candidates) !== "object") {
+      var string_args = Array.prototype.slice.call(arguments, this.length);
+      candidates = string_args;
+    }
+    var matches = [];
+
+    for(i=0; i < candidates.length; i++) {
+      var match = true;
+      var candidate = candidates[i].toLowerCase();
+
+      for(x=0; x < word_to_match.length; x++) {
+        if(word_to_match.length !== candidate.length) {
+          match = false;
+        }
+        if(match) {
+          index = candidate.indexOf(word_to_match[x]);
+          if(word_to_match === candidate || index === -1) {
+            match = false;
+          }
+        }
+      }
+
+      if(match) {
+        matches.push(candidates[i]);
+      }
+    }
+    return matches;
+  }
 };
 
-function Anangram(word) {
-  this.word = word;
+var anagram = function(word) {
+  this.word_to_match = word.toLowerCase();
+  return matcher;
 };
 
-
-module.exports = Anagram;
-
+module.exports = anagram;
